6ef04ebe15cc764c319de0b92646cf6666736a5f
Jooyoung Moon <hckrmoon@gmail.com>
2018-06-05 19:06:43 +0900

    main sfc to react class component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,66 +3,92 @@ import Head from 'next/head'
 import {PageInit} from "../src/components/initializer";
 import {Layout} from "../src/components/layout/Layout";
 
-export default PageInit((): JSX.Element => {
-  return (
-    <Layout>
-      <Head>
-      </Head>
-      <main className="Home">
-        <header className="Header">
-          <h1>
-            <i>Index of&nbsp;</i>
-            <a href="/">hckrmoon/</a>
-          </h1>
-          <a
-            className="single-column"
-            id="toggle"
-            title="click to toggle the view"
-          />
-        </header>
-        <div className="Files">
-          <ul id="files">
-            <li>
-              <a href="/dir" title="dir/" className="dir">
-                dir/
-              </a>
-            </li>
-            <li>
-              <a href="/dir2" title="dir2/" className="dir">
-                dir2/
-              </a>
-            </li>
-            <li>
-              <a href="/dir3" title="dir3/" className="dir">
-                dir3/
-              </a>
-            </li>
-            <li>
-              <a href="/file" title="file" className="png">
-                file
-              </a>
-            </li>
-            <li>
-              <a href="/file2" title="dir3/" className="file">
-                file2
-              </a>
-            </li>
-            <li>
-              <a href="/file3" title="file3/" className="file">
-                file3
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="Preview">
+interface IndexProps {
+}
 
-        </div>
-      </main>
-      <style jsx>{`
+interface IndexState {
+}
+
+/**
+ * define Index class inherits React.Component
+ * @React View Component
+ */
+export default PageInit(class extends React.Component<IndexProps, IndexState> {
+
+  /**
+   * Index class constructor method
+   * @constructs
+   * @param {IndexProps} props
+   */
+  constructor(props: IndexProps) {
+    super(props)
+  }
+
+  /**
+   * Index React Component render method
+   * @returns {JSX.Element}
+   */
+  render() {
+    return (
+      <Layout>
+        <Head>
+        </Head>
+        <main className="Home">
+          <header className="Header">
+            <h1>
+              <i>Index of&nbsp;</i>
+              <a href="/">hckrmoon/</a>
+            </h1>
+            <a
+              className="single-column"
+              id="toggle"
+              title="click to toggle the view"
+            />
+          </header>
+          <div className="Files">
+            <ul id="files">
+              <li>
+                <a href="/dir" title="dir/" className="dir">
+                  dir/
+                </a>
+              </li>
+              <li>
+                <a href="/dir2" title="dir2/" className="dir">
+                  dir2/
+                </a>
+              </li>
+              <li>
+                <a href="/dir3" title="dir3/" className="dir">
+                  dir3/
+                </a>
+              </li>
+              <li>
+                <a href="/file" title="file" className="png">
+                  file
+                </a>
+              </li>
+              <li>
+                <a href="/file2" title="dir3/" className="file">
+                  file2
+                </a>
+              </li>
+              <li>
+                <a href="/file3" title="file3/" className="file">
+                  file3
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="Preview">
+
+          </div>
+        </main>
+        <style jsx>{`
         .Files {
           max-width: 690px;
         }
       `}</style>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 })
