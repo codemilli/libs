ee0286ad13593bf26338e8e4b0e98a55f5ecd1af
Jooyoung Moon <hckrmoon@gmail.com>
2018-06-05 19:03:34 +0900

    style change

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,22 +14,55 @@ export default PageInit((): JSX.Element => {
             <i>Index of&nbsp;</i>
             <a href="/">hckrmoon/</a>
           </h1>
-          <a className="single-column" id="toggle" title="click to toggle the view"></a>
+          <a
+            className="single-column"
+            id="toggle"
+            title="click to toggle the view"
+          />
         </header>
-        <div className="Container">
-          <div className="Title">
-            <p>libs</p>
-          </div>
+        <div className="Files">
+          <ul id="files">
+            <li>
+              <a href="/dir" title="dir/" className="dir">
+                dir/
+              </a>
+            </li>
+            <li>
+              <a href="/dir2" title="dir2/" className="dir">
+                dir2/
+              </a>
+            </li>
+            <li>
+              <a href="/dir3" title="dir3/" className="dir">
+                dir3/
+              </a>
+            </li>
+            <li>
+              <a href="/file" title="file" className="png">
+                file
+              </a>
+            </li>
+            <li>
+              <a href="/file2" title="dir3/" className="file">
+                file2
+              </a>
+            </li>
+            <li>
+              <a href="/file3" title="file3/" className="file">
+                file3
+              </a>
+            </li>
+          </ul>
         </div>
-        <style jsx>{`
-          @font-face {
-            font-family: 'rbicon';
-            src: url(chrome-extension://dipiagiiohfljcicegpgffpbnjmgjcnf/fonts/rbicon.woff2) format("woff2");
-            font-weight: normal;
-            font-style: normal;
-          }
-        `}</style>
+        <div className="Preview">
+
+        </div>
       </main>
+      <style jsx>{`
+        .Files {
+          max-width: 690px;
+        }
+      `}</style>
     </Layout>
   )
 })
