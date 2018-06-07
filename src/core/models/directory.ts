c7e31d50700cc9500ea105b6f08a1e0acd499531
Jooyoung Moon <hckrmoon@gmail.com>
2018-06-08 03:10:14 +0900

    entities modeling

---
+++ src/core/models/directory.ts
@@ -0,0 +1,6 @@
+import {Entity} from "./entity";
+
+export class Directory extends Entity {
+  id: number
+
+}
---
+++ src/core/models/entity.ts
@@ -0,0 +1,8 @@
+export class Entity {
+  name: string
+  parent_id?: number
+  user_id?: number
+  created_at: number
+  updated_at: number
+  deleted_at: number
+}
