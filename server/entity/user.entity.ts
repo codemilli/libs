bb96d19406302adc8184aba73b09c533ff7bed15
hckrmoon <hckrmoon@gmail.com>
2018-06-13 03:31:04 +0900

    user entity added

--- server/entity/user.entity.ts
+++ server/entity/user.entity.ts
@@ -1,10 +1,76 @@
-import {Entity} from "typeorm";
+import * as bcrypt from 'bcrypt'
+import {Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 
-@Entity()
+export enum UserAccountType {
+  LOCAL = 'LOCAL',
+  FACEBOOK = 'FACEBOOK',
+}
+
+export enum UserType {
+  USER = 'USER',
+  ADMIN = 'ADMIN'
+}
+
+@Entity('user')
 export class User {
+
+  @PrimaryGeneratedColumn()
   id: number
+
+  @Column({unique: true})
   username: string
-  created_at: Date
-  updated_at: Date
-  deleted_at: Date
+
+  @Column({unique: true})
+  password: string
+
+  @Column({nullable: true})
+  salt: string
+
+  @Column({
+    type: 'varchar',
+    default: UserAccountType.LOCAL
+  })
+  account_type: UserAccountType
+
+  @OneToMany(
+    type => Entity,
+    entity => entity.user
+  )
+  links: Entity[]
+
+  @CreateDateColumn()
+  created_at: string
+
+  @UpdateDateColumn()
+  updated_at: string
+
+  @Column({
+    type: 'datetime',
+    nullable: true
+  })
+  deleted_at: string
+
+  static async hashing(str, salt): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      bcrypt.hash(str, salt, (err, hashed) => {
+        if (err)
+          reject(err)
+        resolve(hashed)
+
+        return hashed
+      })
+    })
+  }
+
+  static async getSalt(): Promise<any> {
+    return await new Promise((resolve, reject) => {
+      bcrypt.genSalt(10, (err, salt) => {
+        if (err)
+          reject(err)
+        resolve(salt)
+
+        return salt
+      })
+    })
+  }
 }
